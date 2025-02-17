3995818a49488bf90531f3aed75b1c53df7d419d
Ibon Azkoitia <ibon@kreatidos.com>
2025-02-17 15:17:52 +0100

    Register the ClickUp app singleton with the api_token and base_url

--- src/LaravelClickupServiceProvider.php
+++ src/LaravelClickupServiceProvider.php
@@ -9,11 +9,18 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class LaravelClickupServiceProvider extends PackageServiceProvider
 {
-
     public function configurePackage(Package $package): void
     {
         $package
             ->name('laravel-clickup')
             ->hasConfigFile();
+    }
+
+    public function packageRegistered(): void
+    {
+        $this->app->singleton(ClickUp::class, fn ($app): \IbonAzkoitia\LaravelClickup\ClickUp => new ClickUp(
+            config('clickup.api_token'),
+            config('clickup.base_url', 'https://api.clickup.com/api/v2')
+        ));
     }
 }
